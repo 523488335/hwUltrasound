aafeca7389109a33ca62c0357b44eded710da22b
meifan <530051607@qq.com>
2018-07-09 02:49:29 +0800

    完成3d点云数据建模

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -196,7 +196,8 @@ $(function() {
 });
 
 //加载3D图像
-$("#3dmodel").on('click',function () {
+
+function load3dmodel() {
 	$.ajax({
 		type:"post",
 		url:"/info/pointSet",
@@ -205,9 +206,367 @@ $("#3dmodel").on('click',function () {
 		dataType:"json",
 		success:function (data) {
 			console.log(data);
+			draw();
+			var renderer;
+			function initRender() {
+
+				renderer = new THREE.WebGLRenderer({
+					antialias: true
+				});
+
+				//renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0)); //设置背景颜色
+
+				renderer.setSize(868, 374);
+
+				$(".model3d").append(renderer.domElement);
+
+			}
+
+			var camera;
+
+			function initCamera() {
+
+				camera = new THREE.PerspectiveCamera(45, 868 / 374, 1, 10000);
+
+				camera.position.set(0, 0, 1000);
+
+			}
+
+			var scene;
+
+			function initScene() {
+
+				scene = new THREE.Scene();
+
+			}
+
+			var light;
+
+			function initLight() {
+
+				scene.add(new THREE.AmbientLight(0x404040));
+
+				light = new THREE.DirectionalLight(0xffffff);
+
+				light.position.set(1, 1, 1);
+
+				scene.add(light);
+
+			}
+
+			var cloud;
+
+			function initModel() {
+
+				//轴辅助 （每一个轴的长度）
+
+				// var object = new THREE.AxesHelper(500);
+
+				// scene.add(object);
+
+				//创建THREE.PointCloud粒子的容器
+
+				var geometry = new THREE.Geometry();
+
+				//创建THREE.PointCloud纹理
+
+				var material = new THREE.PointCloudMaterial({
+					size: 5,
+					vertexColors: true,
+					color: 0xffffff
+				});
+
+
+				//循环将粒子的颜色和位置添加到网格当中
+
+
+
+				// for(var x = -5; x <= 5; x++) {
+                //
+				// 	for(var y = -5; y <= 5; y++) {
+                //
+				// 		var particle = new THREE.Vector3(x * 10, y * 10, 0);
+                //
+				// 		geometry.vertices.push(particle);
+                //
+				// 		geometry.colors.push(new THREE.Color(+randomColor()));
+                //
+				// 	}
+                //
+				// }
+
+				//实例化THREE.PointCloud
+
+				// cloud = new THREE.PointCloud(geometry, material);
+                //
+				// scene.add(cloud);
+
+			}
+
+			//随机生成颜色
+
+			function randomColor() {
+
+				var arrHex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"],
+
+					strHex = "0x",
+
+					index;
+
+				for(var i = 0; i < 6; i++) {
+
+					index = Math.round(Math.random() * 15);
+
+					strHex += arrHex[index];
+
+				}
+
+				return strHex;
+
+			}
+
+			//初始化性能插件
+
+			var stats;
+
+			function initStats() {
+
+				stats = new Stats();
+
+				$(".model3d").append(stats.dom);
+
+			}
+
+			//用户交互插件 鼠标左键按住旋转，右键按住平移，滚轮缩放
+
+			var controls;
+
+			function initControls() {
+
+				controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+				// 如果使用animate方法时，将此函数删除
+
+				//controls.addEventListener( 'change', render );
+
+				// 使动画循环使用时阻尼或自转 意思是否有惯性
+
+				controls.enableDamping = true;
+
+				//动态阻尼系数 就是鼠标拖拽旋转灵敏度
+
+				//controls.dampingFactor = 0.25;
+
+				//是否可以缩放
+
+				controls.enableZoom = true;
+
+				//是否自动旋转
+
+				controls.autoRotate = false;
+
+				//设置相机距离原点的最远距离
+
+				controls.minDistance = 20;
+
+				//设置相机距离原点的最远距离
+
+				controls.maxDistance = 10000;
+
+				//是否开启右键拖拽
+
+				controls.enablePan = true;
+
+			}
+
+			//生成gui设置配置项
+
+			var gui;
+
+			function initGui() {
+
+				//声明一个保存需求修改的相关数据的对象
+
+				gui = {
+
+					"size": 4,
+
+					"transparent": true,
+
+					"opacity": 1,
+
+					"vertexColors": true,
+
+					"color": 0xffffff,
+
+					"sizeAttenuation": true,
+
+					"rotateSystem": false,
+
+					redraw: function() {
+
+						if(cloud) {
+
+							scene.remove(cloud);
+
+						}
+
+						createParticles(gui.size, gui.transparent, gui.opacity, gui.vertexColors, gui.sizeAttenuation, gui.color);
+
+						//设置是否自动旋转
+
+						controls.autoRotate = gui.rotateSystem;
+
+					}
+
+				};
+
+				//var datGui = new dat.GUI();
+
+				//      //将设置属性添加到gui当中，gui.add(对象，属性，最小值，最大值）gui.add(controls, 'size', 0, 10).onChange(controls.redraw);
+				//
+				//      datGui.add(gui, 'transparent').onChange(gui.redraw);
+				//
+				//      datGui.add(gui, 'opacity', 0, 1).onChange(gui.redraw);
+				//
+				//      datGui.add(gui, 'vertexColors').onChange(gui.redraw);
+				//
+				//      datGui.addColor(gui, 'color').onChange(gui.redraw);
+				//
+				//      datGui.add(gui, 'sizeAttenuation').onChange(gui.redraw);
+				//
+				//      datGui.add(gui, 'rotateSystem').onChange(gui.redraw);
+
+				gui.redraw();
+
+			}
+
+			//生成粒子的方法
+
+			function createParticles(size, transparent, opacity, vertexColors, sizeAttenuation, color) {
+
+				//存放粒子数据的网格
+
+				var geom = new THREE.Geometry();
+
+				//样式化粒子的THREE.PointCloudMaterial材质
+
+				var material = new THREE.PointCloudMaterial({
+
+					size: size,
+
+					transparent: transparent,
+
+					opacity: opacity,
+
+					vertexColors: vertexColors,
+
+					sizeAttenuation: sizeAttenuation,
+
+					color: color
+
+				});
+
+				// for (var i = 0; i < data.light; i++){			alert(data.length);
+                //
+				// 	var particle = new THREE.Vector3(data[i].x, data[i].y, data[i].z);
+				// 	geom.vertices.push(particle);
+				// 	var color = new THREE.Color(+randomColor());
+				// 	color.setHSL(color.getHSL().h, color.getHSL().s, Math.random() * color.getHSL().l);
+                //
+				// 	geom.colors.push(color);
+				// }
+
+				// var range = 500;
+
+				for(var i = 0; i < data.length; i++) {
+
+					var particle = new THREE.Vector3(data[i].x, data[i].y, data[i].z);
+
+					geom.vertices.push(particle);
+
+					var color = new THREE.Color(+randomColor());
+
+					//.setHSL ( h, s, l ) h — 色调值在0.0和1.0之间 s — 饱和值在0.0和1.0之间 l — 亮度值在0.0和1.0之间。 使用HSL设置颜色。
+
+					//随机当前每个粒子的亮度
+
+					color.setHSL(1, 1, 1);
+
+					geom.colors.push(color);
+
+				}
+
+				//生成模型，添加到场景当中
+
+				cloud = new THREE.PointCloud(geom, material);
+
+				scene.add(cloud);
+
+			}
+
+			function render() {
+
+				renderer.render(scene, camera);
+
+			}
+
+			//窗口变动触发的函数
+
+			function onWindowResize() {
+
+				camera.aspect = 868 / 374;
+
+				camera.updateProjectionMatrix();
+
+				render();
+
+				renderer.setSize(868, 374);
+
+			}
+
+			function animate() {
+
+				//更新控制器
+
+				controls.update();
+
+				render();
+
+				//更新性能插件
+
+				//stats.update();
+
+				requestAnimationFrame(animate);
+
+			}
+
+			function draw() {
+
+				initRender();
+
+				initScene();
+
+				initCamera();
+
+				initLight();
+
+				//initModel();
+
+				initControls();
+
+				//initStats();
+
+				initGui();
+
+				animate();
+
+				window.onresize = onWindowResize;
+
+			}
 		}
 	});
-})
+}
 
 // 双向数据绑定
 function ViewModel() {
